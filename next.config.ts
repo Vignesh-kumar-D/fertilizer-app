466f0be84cfef91c231cae0c33094ca569f3d54d
legendvi <dvigneshkumar3@gmail.com>
2025-02-26 18:17:20 +0530

    added serwist

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
-import type { NextConfig } from "next";
+/* eslint-disable @typescript-eslint/no-require-imports */
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require('next/constants');
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {(phase: string, defaultConfig: import("next").NextConfig) => Promise<import("next").NextConfig>} */
+module.exports = async (phase: unknown) => {
+  /** @type {import("next").NextConfig} */
+
+  // Your current or future configuration
+
+  const nextConfig = {};
+
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withSerwist = (await import('@serwist/next')).default({
+      swSrc: 'src/service-worker.ts',
+      swDest: 'public/sw.js',
+      reloadOnOnline: true,
+    });
+    return withSerwist(nextConfig);
+  }
+
+  return nextConfig;
 };
-
-export default nextConfig;
